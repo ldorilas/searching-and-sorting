a4ed36b97df6cac90b810214260706b95a494a5d
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-02-28 09:20:07 -0500

    Update SearchAndSort.java

--- src/SearchAndSort.java
+++ src/SearchAndSort.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class SearchAndSort {
 	
 	/**
@@ -5,8 +6,29 @@ public class SearchAndSort {
 	 * 
 	 * @param args
 	 */
+	public void intBubbleSort (int[] intlist) {
+		//some stuff
+	}
 	
+	public void strBubbleSort (String[] strlist) {
+		//some stuff
+	}
+	public static Scanner in = new Scanner(System.in);
 	public static void main(String[] args) {
+		SearchAndSort sorting = new SearchAndSort();
+
+		System.out.println("What algorithm would you like to execute?");
+		String choice = in.nextLine();
+		switch (choice) {
+		case "intbubble":
+			sorting.intBubbleSort(intlist);
+			break;
+		case "stringbubble":
+			sorting.strBubbleSort(strlist);
+		}
 		
+		System.out.println("What type of data?");
+		String data = in.nextLine();
+		System.out.println("How is it stored?");
 	}
 }
